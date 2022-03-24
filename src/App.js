94f184ee209dfce1fe4c81667f2120c264b265c2
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-23 23:57:16 -0400

    stable app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
+  let [data, setData] = useState();
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(err => console.error(err))
+  })
   return (
     <div className="App">
      
